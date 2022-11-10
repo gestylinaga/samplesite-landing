097a239c45c87bb91a3e1184584b12e9ccd69b94
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-11-09 19:34:43 -0800

    fixed reactivity, updated navbar, & started animations

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Sample Site - Landing 
+console.log("by: gesty 🏄 ");
+
+// IntersectionObserver for animations
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+    entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }  
+  });
+});
+
+const hiddenLeft = document.querySelectorAll('.hidden-left');
+hiddenLeft.forEach((el) => observer.observe(el));
+const hiddenRight = document.querySelectorAll('.hidden-right');
+hiddenRight.forEach((el) => observer.observe(el));
+const hiddenBelow = document.querySelectorAll('.hidden-below');
+hiddenBelow.forEach((el) => observer.observe(el));
+const hiddenAbove = document.querySelectorAll('.hidden-above');
+hiddenAbove.forEach((el) => observer.observe(el));
